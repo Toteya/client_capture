09bf395b25cce4940de7e3280ab78be366d22696
Toteya <111172685+Toteya@users.noreply.github.com>
2025-04-07 12:24:07 +0200

    added functionality: adding a new client

--- client/static/scripts/clients.js
+++ client/static/scripts/clients.js
@@ -6,16 +6,25 @@ $(document).ready(function() {
     type: 'GET',
     url: 'http://localhost:5001/api/clients',
     contentType: 'application/json',
-    success: (clients) => {
-      generateClientTable(clients);
+    success: (response) => { 
+      const clients = response;
+      generateClientTable(clients)
     },
     error: (error) => {
-      console.error('Error fetching clients:', error);
+      console.error('Error fetching clients:', error.state);
+      $container.append('<p style="color: red">Error fetching clients. Please try again later.</p>');
     }
   });
+
   
   // Generate the client list
   const generateClientTable = (clients) => {
+
+    if (clients.length === 0) {
+      $container.append('<p>No clients found.</p>');
+      return;
+    }
+
     const $table = $('<table>', { border: 1, id: 'clientTable' });
     const $thead = $('<thead>').append(`
       <tr>
@@ -38,4 +47,42 @@ $(document).ready(function() {
     $table.append($thead, $tbody);
     $container.append($table);
   }
+
+  // Creating a new client
+  const $addClientButton = $('#addClientButton');
+  const $formContainer = $('#clientFormContainer');
+  const $form = $('#clientForm');
+  const $cancelButton = $('#cancelButton');
+
+  $addClientButton.on('click', () => {
+    $formContainer.show();
+
+    $form.on('submit', (event) => {
+      event.preventDefault();
+
+      const clientName = $('#name').val();      
+      const clientCode = 194;
+
+      if (clientName) {
+        $.ajax({
+          type: 'POST',
+          url: 'http://localhost:5001/api/clients',
+          contentType: 'application/json',
+          data: JSON.stringify({ name: clientName, client_code: clientCode }),
+          success: (response) => {
+            alert('Client added successfully!');
+            location.reload(); // Reload the page to see the new client
+          },
+          error: (error) => {
+            console.error('Error adding client:', error);
+          }
+        });
+      }
+    });
+
+    $cancelButton.on('click', () => {
+      $formContainer.hide();
+      $form[0].reset();
+    });
+  });
 });
