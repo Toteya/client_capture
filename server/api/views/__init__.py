120a9202ad3fdc3037aa892ce357e9aec6a62a20
Toteya <111172685+Toteya@users.noreply.github.com>
2025-04-22 16:08:36 +0200

    import contacts to Blueprint

--- server/api/views/__init__.py
+++ server/api/views/__init__.py
@@ -8,4 +8,4 @@ from flask import Blueprint
 api_views = Blueprint("api_views", __name__, url_prefix="/api")
 
 from server.api.views.clients import *
-# from server.api.views.contacts import *
+from server.api.views.contacts import *
