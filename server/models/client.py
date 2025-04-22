474f78e3b218ffafa5dc079370d9ab11a8b26299
Toteya <111172685+Toteya@users.noreply.github.com>
2025-04-22 16:27:41 +0200

    updated models relationship between clients and contacts -> Many-to-Many

--- server/models/client.py
+++ server/models/client.py
@@ -1,12 +1,19 @@
 """
 client: Contains client model implementation
 """
-from sqlalchemy import Column, String
+from sqlalchemy import Column, ForeignKey, String, Table
 from sqlalchemy.orm import relationship
 from server.models.base import BaseModel
 from server.models.base import Base
 from server.models.base import Column
 
+
+client_contact_assoc = Table(
+    'client_contact',
+    Base.metadata,
+    Column('client_id', String(60), ForeignKey('clients.id'), primary_key=True),
+    Column('contact_id', String(60), ForeignKey('contacts.id'), primary_key=True)
+)
 
 class Client(BaseModel, Base):
     """ Client class that inherits from BaseModel and Base
@@ -17,4 +24,4 @@ class Client(BaseModel, Base):
     name = Column('name', String(128), nullable=False)
     client_code = Column('client_code', String(128), nullable=False)
 
-    contacts = relationship('Contact', back_populates='client')
+    contacts = relationship('Contact', back_populates='clients', secondary=client_contact_assoc)
--- server/models/contact.py
+++ server/models/contact.py
@@ -4,6 +4,7 @@ contact: Contains contact model implementation.
 from sqlalchemy import Column, ForeignKey, String
 from server.models.base import Base, BaseModel
 from sqlalchemy.orm import relationship
+from server.models.client import client_contact_assoc
 
 
 class Contact(BaseModel, Base):
@@ -17,4 +18,4 @@ class Contact(BaseModel, Base):
     email = Column('email', String(128), unique=True, nullable=False)
     client_id = Column('client_id', String(36), ForeignKey('clients.id'), nullable=True)
 
-    client = relationship('Client', back_populates='contacts')
+    clients = relationship('Client', back_populates='contacts', secondary=client_contact_assoc)
