1b5db6c10743767e7ba298dabe97b6ca5fabe1f1
Toteya <111172685+Toteya@users.noreply.github.com>
2025-04-22 16:42:10 +0200

    added endpoint functionality: link contact to client

--- server/api/views/clients.py
+++ server/api/views/clients.py
@@ -2,8 +2,10 @@
 API views related to the client data
 """
 from flask import jsonify, request
+from sqlalchemy.exc import IntegrityError
 from server.models import storage
 from server.models.client import Client
+from server.models.contact import Contact
 from server.api.views import api_views
 
 @api_views.route('/clients', methods=['GET'], strict_slashes=False)
@@ -56,3 +58,25 @@ def delete_client(client_id):
     client.delete()
     storage.close()
     return jsonify({}), 200
+
+@api_views.route('/clients/<client_id>/<contact_id>', methods=['PUT'], strict_slashes=False)
+def link_contact_to_client(client_id, contact_id):
+    """
+    Links a contact to a specific client by ID
+    """
+    client = storage.get(Client, client_id)
+    if not client:
+        return jsonify({"error": "Client not found"}), 404
+
+    contact = storage.get(Contact, contact_id)
+    if not contact:
+        return jsonify({"error": "Contact not found"}), 404
+
+    client.contacts.append(contact)
+    try:
+        client.save()
+    except IntegrityError:
+        storage.close()
+        return jsonify({"error": "Contact already linked to this client"}), 400
+    client.name
+    return jsonify(client.to_dict()), 200
