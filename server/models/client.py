b0ba040a281250713ea0c2c7d3e2b6de4f0e1037
Toteya <111172685+Toteya@users.noreply.github.com>
2025-04-29 10:58:30 +0200

    updated client model to automatically generate client codes on initialisation

--- server/models/client.py
+++ server/models/client.py
@@ -25,3 +25,38 @@ class Client(BaseModel, Base):
     client_code = Column('client_code', String(128), nullable=False)
 
     contacts = relationship('Contact', back_populates='clients', secondary=client_contact_assoc, lazy='joined')
+
+    def __init__(self, *args, **kwargs):
+        """ Initializes the Client instance
+        """
+        self.client_code = self.generate_client_code(kwargs.get('name'))
+        super().__init__(*args, **kwargs)
+    
+    def generate_client_code(self, name):
+        """ Generates a unique client code
+        """
+        from server.models import storage
+        prefix = ''
+        if len(name.split()) >= 3:
+            prefix = f"{name.split()[0][0].upper()}{name.split()[1][0].upper()}{name.split()[2][0].upper()}"
+        elif len(name.split()) == 2:
+            prefix = f"{name.split()[0][0].upper()}{name.split()[0][1]}{name.split()[1][0].upper()}"
+        else:
+            prefix = f"{name[0].upper()}{name[1].upper()}{name[2].upper()}"
+
+        codes = storage.get_client_codes(prefix)
+        suffix = 1
+        new_code = f"{prefix}00{suffix}"
+
+        while True:
+            if new_code in codes:
+                suffix += 1
+                if suffix < 10:
+                    new_code = f"{prefix}00{suffix}"
+                elif suffix < 100:
+                    new_code = f"{prefix}0{suffix}"
+                else:
+                    new_code = f"{prefix}{suffix}"
+            else:
+                break
+        return new_code
